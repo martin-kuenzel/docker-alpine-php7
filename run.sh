f2701ca430e466131b54fb155f6fae2d2a506d77
mlkone <martin.kuenzel@hs-bochum.de>
2019-03-05 02:48:16 +0100

    Änderung der Verzeichnisstruktur

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ COW "rebuilding and starting Project $PROJECT_NAME"
 #( rsync -avup "custom_config/.tmp"/ "src/FB.Switch/data"/.cache; )
 
 ( docker stop "$(DOCKER_PROCNAME)" ) >/dev/null 2>&1
-COW "Hello $USER. I'm afraid I have to tell you that container $(DOCKER_PROCNAME) has been killed.We do not exactly know what happened yet and you are the only one who can find out... Farewell!"
+COW "Hello $USER. I'm afraid I have to tell you that container $(DOCKER_PROCNAME) is no longer with us. We do not exactly know what happened yet and you are the only one who can find out... Farewell!"
 
 ( docker rm $(docker ps -af label="$PROJECT_NAME" -f status='exited' -f status='created' |awk '{print $1}' ) )   >/dev/null 2>&1
 ( docker rm "$(DOCKER_PROCNAME)" )  >/dev/null 2>&1
