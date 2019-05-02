f5df7a74d644d12a8e11fad2953a59f2aae94b05
mlkone <martin.kuenzel@hs-bochum.de>
2019-05-02 13:56:13 +0200

    fixes in structure, and additional scripts

--- custom_config/bin/backup_data.sh
+++ custom_config/bin/backup_data.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+CTIME=5
+[ $CTIME -lt 2 ] && CTIME=2;
+
 while true; do
-  [ $(( $(date +%s) % 5 )) -eq 0 ] && {
+  [ $(( $(date +%s) % $CTIME )) -eq 0 ] && {
     rsync -avup "/var/www/localhost/htdocs/data/" "/var/www/localhost/htdocs/.cache";
+    sleep $(( $CTIME - 2 ))
   }
 done
+exit 0;
--- custom_config/bin/run_timers.sh
+++ custom_config/bin/run_timers.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+CTIME=60
+[ $CTIME -lt 2 ] && CTIME=2;
+
 while true; do
-  [ $(( $(date +%s) % 60 )) -eq 0 ] && {
+  [ $(( $(date +%s) % $CTIME )) -eq 0 ] && {
     wget -q http://127.0.0.1/index.php?timerrun -O - >/dev/null 2>&1;
+    sleep $(( $CTIME / 2 ))
   }
 done
 exit 0;
