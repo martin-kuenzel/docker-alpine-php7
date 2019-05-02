45549124d125eddf1a9228edd836a857fa02ea91
mlkone <martin.kuenzel@hs-bochum.de>
2019-05-02 13:42:50 +0200

    fixes in structure, and additional scripts

--- custom_config/bin/backup_data.sh
+++ custom_config/bin/backup_data.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 while true; do
-  [ $(( $(date +%s) % 60 )) -eq 0 ] && {
+  [ $(( $(date +%s) % 5 )) -eq 0 ] && {
     rsync -avup "/var/www/localhost/htdocs/data/" "/var/www/localhost/htdocs/.cache";
   }
 done
